3a679d3cd649bd98d5c40334f4dca16d42d7afca
zhaowei <1512763623@qq.com>
2022-02-22 16:13:03 +0800

    demoVue2

---
+++ demoVue2/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div>{{address}}</div>
+    <button @click="connectWallet">连接钱包</button>
+    <button @click="quote">报价quote</button>
+    <div class="">{{ quoteValue }}</div>
+    <button @click="swap">交易swap</button>
+  </div>
+</template>
+
+<script>
+import SwapSdk, { Api, units } from 'openocean-api-sdk';
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data () {
+    return {
+      address: null,
+      quoteValue: null
+    }
+  },
+  methods: {
+    connectWallet () {
+      debugger
+    },
+    quote () {
+
+    },
+    swap () {
+
+    },
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button{ display: block;}
+</style>
--- test/sdk.ts
+++ test/sdk.ts
@@ -1,5 +1,6 @@
 
 import SwapSdk, { Api, units } from '../src/index';
+// import SwapSdk, { Api, units } from 'openocean-api-sdk';
 
 async function init() {
   let sdk = new SwapSdk()
